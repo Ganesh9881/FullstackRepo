7e0de5c65ed10f7f9fbd5c02afa86bdbae15e5c0
GANESH <GANESH@192.168.1.71>
2025-08-12 00:21:23 +0530

    Commit from Developer_1

--- Java_Project/src/com/nit/test/Test.java
+++ Java_Project/src/com/nit/test/Test.java
@@ -4,6 +4,12 @@ public class Test {
 	
 	public static void main(String[]args)
 	{
+		int a=20;
+		int b=30;
+		
+		int c=a+b;
+		System.out.println("The addition of two numbers:-" +c);
+		
 		
 	}
 
